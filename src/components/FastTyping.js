152feecefdc397bcdc77498f268479b8021eb94c
Ori <ori@example.com>
2024-05-30 20:48:47 +0300

    init branch

---
+++ src/components/FastTyping.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+const chars = "abcdefghijklmnopqrstuvwxyz ";
+
+const FastTyping = () => {
+    const [char, setChar] = useState("")
+    const [needed, setNeeded] = useState("F")
+    const [color, setColor] = useState("green")
+
+    const handleType = (e) => {
+        if (needed === e.target.value) {
+            setColor("green")
+            let newChar = Math.floor(Math.random() * chars.length) +1;
+            newChar = chars[newChar];
+            setNeeded(newChar);
+
+        } else {
+            setColor("red")
+        }
+    }
+    return (
+        <div>
+            <h1> Wellcome to FastTyping.com </h1>
+            <h3> press: {needed} </h3>
+
+            <input style={{ backgroundColor: color }}
+                value={char} onChange={handleType} />
+        </div>
+    )
+}
+
+export default FastTyping
--- src/components/SiteRoutes.js
+++ src/components/SiteRoutes.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import AddProduct from './AddProduct/AddProduct'
 import Products from './Products/Products'
 import DeleteProduct from './DeleteProduct/DeleteProduct'
+import FastTyping from './FastTyping'
 
 const SiteRoutes = () => {
   return (
@@ -14,6 +15,7 @@ const SiteRoutes = () => {
             <Route path='/add' element={<AddProduct/>}/>
             <Route path='/delete' element={<DeleteProduct/>}/>
             <Route path='/delete/:id' element={<DeleteProduct/>}/>
+            <Route path='fast-typing' element={<FastTyping/>}/>
         </Routes>
     </div>
   )
